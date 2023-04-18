582eb03df5bdcd9e733de8b15ea035ed4afb3591
Quique Vázquez Peñamaría <vazquez.penamaria.enrique.ald@gmail.com>
2023-04-18 17:10:43 +0200

    Añadido Tema 7

---
+++ 07-Objetos/js/09-app.js
@@ -0,0 +1,23 @@
+/*
+DIFERENCIA ENTRE FREEZE Y SEAL: FREEZE TE DEJA EL OBJETO TAL CUAL ESTÁ PERO 
+SEAL DEJA MODIFICAR LAS LLAVES EXISTENTES DE UN OBJETO. NO PUDIENDO AÑADIR 
+NUEVAS NI ELIMINARLAS.
+*/
+
+"use strict";
+
+const producto = {
+    nombre: "Monitor 20 pulgadas",
+    precio: 300,
+    disponible: true
+}
+
+Object.seal(producto);
+
+producto.disponible = false;
+// producto.imagen = "imagen.jpg"
+// delete producto.precio;
+
+console.log(producto);
+
+console.log(Object.isSealed(producto));
